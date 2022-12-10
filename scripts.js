7f23817443b4ceaf503208dab5837f38398174b0
Shanel Beebe <shanelbeebe@icloud.com>
2022-12-10 13:16:08 -0700

    updated links, profile image and some css

--- scripts.js
+++ scripts.js
@@ -1,61 +1,111 @@
 const app = {};
 
+app.projectImages = [
+  {
+    src: "floataMU.jpg",
+    alt: "Floata logo business card mockup",
+    group: "logos"},
+  {
+    src: "autonomeMU.jpg",
+    alt: "Autonome logo stationary mockup",
+    group: "logos"},
+  {
+    src: "logosPage.jpg",
+    alt: "Grid of black and white logo designs",
+    group: "logos"},
+  {
+    src: "dnaMU.jpg",
+    alt: "DNA Athletics logo business card mockup",
+    group: "logos"},
+  {
+    src: "clothMU.jpg",
+    alt: "Floata logo business card mockup",
+    group: "logos"},
+  {
+    src: "flintFlameMU.jpg",
+    alt: "Flint & Flame logo business card mockup",
+    group: "logos"},
+  {
+    src: "artHouseMU.jpg",
+    alt: "Arthouse logo stationary mockup",
+    group: "branding"},
+  {
+    src: "artHouseTheatreMU",
+    alt: "Arthouse logo Theatre entrance mockup",
+    group: "branding"},
+  {
+    src: "liftBusCardMU.jpg",
+    alt: "Lift Hot Air Balloons logo business card mockup",
+    group: "branding"},
+  {
+    src: "liftPhoneCaseMU.jpg",
+    alt: "Lift Hot Air Balloons logo phone case mockup",
+    group: "branding"},
+  {
+    src: "prattNormanMU.jpg",
+    alt: "Pratt Norman logo stationary mockup",
+    group: "branding"},
+  {
+    src: "prattBusCardMU.jpg",
+    alt: "Pratt Norman logo business card mockup",
+    group: "branding"},
+  {
+    src: "nationMU.jpg",
+    alt: "Nation magazaine layout mockup",
+    group: "layout"},
+  {
+    src: "aliceMU.jpg",
+    alt: "Nation Magazine editorial design mockup",
+    group: "layout"},
+  {
+    src: "powwowMU.jpg",
+    alt: "Nation Magazine editorial design mockup",
+    group: "layout"},
+  {
+    src: "natlReportMU.jpg",
+    alt: "National Report layout design mockups",
+    group: "layout"},
+  {
+    src: "dblExpBeach.jpg",
+    alt: "Double exposure image of a woman blended with beach scene",
+    group: "photoshop"},
+  {
+    src: "smokeEffectHair.jpg",
+    alt: "Photoshopped image of a woman with hair dissolving into smoke",
+    group: "photoshop"},
+  {
+    src: "dblExpBirds.jpg",
+    alt: "Double exposure image of a man blended with trees and birds",
+    group: "photoshop"},
+  {
+    src: "smokeEffectDancer.jpg",
+    alt: "Photoshopped image of a man dissoliving into smoke",
+    group: "photoshop"}
+]
+
 // displays logo design gallery in projects gallery div
-$("#logos").on("click", (e) => {
+$images = app.projectImages
+$(".projectsList ul li").each(function () {
+  $(this).on('click', (e) => {
+    $idx=0
+    $(".projectsGallery").html('')
+    $groupImgArr = []
+    $.each($images, i => {
+      console.log($images[i].group, this.id);
+      if ($images[i].group === this.id) {
+        $groupImgArr.push($images[i])
+        $idx++
+        console.log($idx, $groupImgArr)
+        $(".projectsGallery").append(
+          $(` <img id='gallery${$idx}' class="galleryImg" data-listnum="${$idx-1}" src="./assets/${$groupImgArr[$idx-1].src}" alt="${$groupImgArr[$idx-1].alt}" tabIndex="0"/>`)
+        )
+      } else {
+        return
+      }
+    })
+  })
+})
 
-  e.preventDefault();
-  $(".projectsGallery").html(
-    ` <img id="gallery1" class="galleryImg" data-listnum="0" src="./assets/floataMU.jpg" alt="Floata logo business card mockup" tabIndex="0"/>
-    <img id="gallery2" class="galleryImg" data-listnum="1" src="./assets/autonomeMU.jpg" alt="Autonome logo stationary mockup" tabIndex="0"/>
-    <img id="gallery3" class="galleryImg" data-listnum="2" src="./assets/logosPage.jpg" alt="Grid of black and white logo designs" tabIndex="0"/>
-    <img id="gallery4" class="galleryImg" data-listnum="3" src="./assets/dnaMU.jpg" alt="DNA Athletics logo business card mockup" />
-    <img id="gallery5" class="galleryImg" data-listnum="4" src="./assets/clothMU.jpg" alt="Cloth clothing logo business card mockup" tabIndex="0"/>
-    <img id="gallery6" class="galleryImg" data-listnum="5" src="./assets/flintFlameMU.jpg" alt="Flint & Flame logo business card mockup" tabIndex="0"/>`
-  );
-  
-});
-
-// displays branding design gallery in projects gallery div
-$("#branding").on("click", (e) => {
-
-  e.preventDefault();
-  $(".projectsGallery").html(
-    `<img id="gallery1" class="galleryImg" data-listnum="0" src="./assets/artHouseMU.jpg" alt="Arthouse logo stationary mockup" tabIndex="0"/>
-    <img id="gallery2" class="galleryImg" data-listnum="1" src="./assets/artHouseTheatreMU" alt="Arthouse logo Theatre entrance mockup" tabIndex="0"/>
-    <img id="gallery3" class="galleryImg" data-listnum="2" src="./assets/liftBusCardMU.jpg" alt="Lift Hot Air Balloons logo business card mockup" tabIndex="0"/>
-    <img id="gallery4" class="galleryImg" data-listnum="3" src="./assets/liftPhoneCaseMU.jpg" alt="Lift Hot Air Balloons logo phone cases mockup" tabIndex="0"/>
-    <img id="gallery5" class="galleryImg" data-listnum="4" src="./assets/prattNormanMU.jpg" alt="Pratt Norman logo stationary mockup" tabIndex="0"/>
-    <img id="gallery6" class="galleryImg" data-listnum="5" src="./assets/prattBusCardMU.jpg" alt="Pratt Norman logo business card mockup" tabIndex="0"/>
-    `
-  );
-  
-});
-
-// displays layout design gallery in projects gallery div
-$("#layout").on("click", (e) => {
-
-  e.preventDefault();
-  $(".projectsGallery").html(
-    `<img id="gallery1" class="galleryImg layout" data-listnum="0" src="./assets/nationMU.jpg" alt="Nation magazaine layout mockup" tabIndex="0"/>
-    <img id="gallery2" class="galleryImg layout" data-listnum="1" src="./assets/aliceMU.jpg" alt="Alice In Wonderland book design mockup" tabIndex="0"/>
-    <img id="gallery3" class="galleryImg layout" data-listnum="2" src="./assets/powwowMU.jpg" alt="Nation Magazine editorial design mockup" tabIndex="0"/>
-    <img id="gallery4" class="galleryImg layout" data-listnum="3" src="./assets/natlReportMU.jpg" alt="National Report layout design mockups" tabIndex="0"/>`
-  );
-  
-  });
-
-// displays photoshop design gallery in projects gallery div
-$("#photoshop").on("click", (e) => {
-
-  e.preventDefault();
-  $(".projectsGallery").html(
-    `<img id="gallery1" class="galleryImg" data-listnum="0" src="./assets/dblExpBeach.jpg" alt="Double exposure image of a woman blended with beach scene" tabIndex="0"/>
-    <img id="gallery2" class="galleryImg" data-listnum="1" src="./assets/smokeEffectHair.jpg" alt="Photoshopped image of a woman with hair dissolving into smoke" tabIndex="0"/>
-    <img id="gallery3" class="galleryImg" data-listnum="2" src="./assets/dblExpBirds.jpg" alt="Double exposure image of a man blended with trees and birds" tabIndex="0"/>
-    <img id="gallery4" class="galleryImg" data-listnum="3" src="./assets/smokeEffectDancer.jpg" alt="Photoshopped image of a man dissoliving into smoke" tabIndex="0"/>`
-  );
-  
-});
 
 // Adding open class to modal so that it appears on the page
 app.openModal = () => {
